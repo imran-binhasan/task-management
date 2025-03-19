01f73fe830b128fd39f50158ab79b823542ba93d
imran-binhasan <imranbinhasan.work@gmail.com>
2025-03-19 10:06:07 +0600

    add filter method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,17 +6,22 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { createTaskDto } from './dto/create-task.dto';
+import { getTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterDto:getTasksFilterDto): Task[] {
+    if(Object.keys(filterDto).length){
+    return this.taskService.getTasksWithFilters(filterDto)
+    }
     return this.taskService.getAllTasks();
   }
 
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,12 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v7 as uuid } from 'uuid';
 import { createTaskDto } from './dto/create-task.dto';
+import { getTasksFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: getTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+    if (status) {
+      tasks = tasks.filter((tasks) => tasks.status === status);
+    }
+    if (search) {
+      tasks = tasks.filter((tasks) => {
+        if (
+          tasks.title.includes(search) ||
+          tasks.description.includes(search)
+        ) {
+          return true
+        }
+        return false
+      });
+    }
+    return tasks;
   }
 
   getTaskById(id: string): Task {
@@ -31,23 +52,21 @@ export class TasksService {
     return task;
   }
 
-  deleteTaskById(id:string):void{
+  deleteTaskById(id: string): void {
     const idx = this.tasks.findIndex((task) => task.id === id);
-    if(idx !== -1){
-        this.tasks.splice(idx,1);
+    if (idx !== -1) {
+      this.tasks.splice(idx, 1);
     }
-}
+  }
 
-updateTaskById(id:string,body:Task):Task{
-    let oldTask = this.tasks.find((task)=> task.id === id);
-    if(!oldTask){
-        throw new Error('error')
+  updateTaskById(id: string, body: Task): Task {
+    let oldTask = this.tasks.find((task) => task.id === id);
+    if (!oldTask) {
+      throw new Error('error');
     }
-     oldTask.title = body.title;
-     oldTask.description = body.description;
-     oldTask.status =body.status;
-     return oldTask;
-
-}
-
+    oldTask.title = body.title;
+    oldTask.description = body.description;
+    oldTask.status = body.status;
+    return oldTask;
+  }
 }
